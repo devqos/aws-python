38ec9c33c55a435fea52561932c536ed7e99efde
devqos <104697697+devqos@users.noreply.github.com>
2023-05-18 18:09:03 +0200

    Test ci pipeline

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,5 +5,8 @@ class TestApp(unittest.TestCase):
     def test_add_two_numbers(self):
         self.assertEqual(2 + 2, 4)
 
+    def test_subtract_two_numbers(self):
+        self.assertEqual(2 - 2, 0)
+
     if __name__ == '__main__':
         unittest.main()
